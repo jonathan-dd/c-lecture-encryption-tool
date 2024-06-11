d485033de6bab4ed4588513ae19c9db9ddfa27db
jonathan-dd <jonathan.dechow@proton.me>
2024-06-11 18:54:43 +0200

    set up basic program structure without encryption or decryption capabilities

--- main.c
+++ main.c
@@ -5,10 +5,57 @@ Authors: Silas Hausdörfer, Jonathan Dechow
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <string.h>
 
+#define MAX_PATH_LENGTH 260
 
-//This function checks if the terminal the is use has admin privileges
-BOOL IsAdmin() {
+// Declare functions:
+BOOL isAdmin();
+void encrypt();
+void decrypt();
+unsigned char* readFileAsBinary();
+char* getAbsolutePath(const char* relativPath);
+
+int main(int argc, char *argv[]){
+
+    // We check for admin rights to avoid needing a verification or password system
+    // !!!Not final maybe it is unnecesary
+    if(!isAdmin()){
+        printf("This application requires adminisrative privileges.\nPlease use a terminal with admin privileges.");
+        return 1;
+    }
+
+    // Check if the number of arguments the user has given is valid
+    if(argc != 3){
+        printf("Please provide two arguments\nIf you need help use the argument help");
+        return 1;
+    }
+
+    //convert whatever path was given to an absolute path
+    //Currently does't support Home directory relative paths
+    const char* filePath = argv[2];
+    char *absolutePath = getAbsolutePath(filePath);
+
+    // Check which argument was given and execute whatever functionality was requested
+    if (strcmp(argv[1], "help") == 0){
+        printf("Help section\n------------\n");
+        printf("Possible arguments:\n  help\n  encrypt <filepath>\n  decrypt <filepath>");
+    } else if (strcmp(argv[1], "encrypt") == 0)
+    {
+        encrypt(absolutePath);
+    } else if (strcmp(argv[1], "decrypt") == 0)
+    {
+        decrypt();
+    }else{
+        printf("Unknown argument");
+        return 1;
+    }
+
+    return 0;
+}
+
+//This function checks if the terminal has admin privileges
+BOOL isAdmin() {
     BOOL isAdmin = FALSE;
     HANDLE token = NULL;
     DWORD tokenInfoLength = 0;
@@ -27,17 +74,40 @@ BOOL IsAdmin() {
     return isAdmin;
 }
 
-int main(){
+// Encrypt a file
+void encrypt(char* filePath){
+    char* fileData = readFileAsBinary(filePath);
+}
 
-    // We check for admin rights to avoid needing a verification or password system
-    if(!IsAdmin()){
-        printf("This application requires adminisrative privileges.\n Please use a terminal with admin privileges.");
-        return 1;
+// Decrypt a file
+void decrypt(){
+
+}
+
+// Reads a file and returns it as binary
+unsigned char* readFileAsBinary(const char* filePath){
+    printf("File Path: %s\n", filePath);
+
+    // not yet implemented
+
+    return NULL;
+}
+
+// converts a relative path to an absolute path
+char* getAbsolutePath(const char* relativePath){
+    char *absolutePath = (char *)malloc(MAX_PATH_LENGTH);
+    //Error handling for malloc
+    if(!absolutePath){
+        fprintf(stderr, "Memory allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+    // converting relative to absolute path and error handling for _fullpath
+    if (_fullpath(absolutePath, relativePath, MAX_PATH_LENGTH) == NULL){
+        fprintf(stderr, "Error converting to absolute path\n");
+        free(absolutePath);
+        exit(EXIT_FAILURE);
     }
 
-    //main encryption/decryption code starts here
-
-
-
-    return 0;
+    // Return the absolute path if everything went well
+    return absolutePath;
 }
