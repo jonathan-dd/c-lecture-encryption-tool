0cd8c178b86107bac307495f647322487403f99c
jonathan-dd <jonathan.dechow@proton.me>
2024-07-23 12:51:27 +0200

    Revert "moved code from main. to external files"
    This reverts commit e92720e5877802323a4328cdb718c1fb00398122.

--- file.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILE_H
-#define FILE_H
-
-unsigned char *readFileAsBinary(const char *filePath, size_t *fileSize);
-void writeFileFromBinary(unsigned char *fileData, size_t *fileSize, char *filePath, char encryptOrDecrypt);
-char *getAbsolutePath(const char *relativPath);
-char *editFilePath(char *filePath, char encryptOrDecrypt);
-
-#endif
